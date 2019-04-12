1dc71d9100d4335c514109c9f05b5f9d327bcb58
Jonathan Lancaster <jcast90@me.com>
2019-04-12 07:58:08 -0700

    Updates

---
+++ precache-manifest.910ef0eeabc715a7396ae4a8ff9bdeda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d433318c5d761abc7fec",
+    "url": "/cat-generator/build/static/static/css/main.41e1e862.chunk.css"
+  },
+  {
+    "revision": "d433318c5d761abc7fec",
+    "url": "/cat-generator/build/static/static/js/main.394033c2.chunk.js"
+  },
+  {
+    "revision": "48e8ce58a074eae010be",
+    "url": "/cat-generator/build/static/static/js/runtime~main.a147dda7.js"
+  },
+  {
+    "revision": "a0dcbc6bffb8bdbda6aa",
+    "url": "/cat-generator/build/static/static/js/2.fa5dcf90.chunk.js"
+  },
+  {
+    "revision": "814277870f79bf6c75bc88e636176673",
+    "url": "/cat-generator/build/static/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cat-generator/build/precache-manifest.788d1dfd6185a87ea3d4adde5bced8ae.js"
+  "/cat-generator/build/static/precache-manifest.910ef0eeabc715a7396ae4a8ff9bdeda.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cat-generator/build/index.html", {
+workbox.routing.registerNavigationRoute("/cat-generator/build/static/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
